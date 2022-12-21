19aef7cba61457327861f670c4bf2a0b73cc1a8c
IvanovMaxim2000 <macik_iv2000@mail.ru>
2022-12-21 11:13:04 +0300

    Update TiltWindow.cs

--- My project/Assets/MainMenu/TiltWindow.cs
+++ My project/Assets/MainMenu/TiltWindow.cs
@@ -4,9 +4,9 @@ public class TiltWindow : MonoBehaviour
 {
 	public Vector2 range = new Vector2(5f, 3f);
 
-	Transform mTrans;
-	Quaternion mStart;
-	Vector2 mRot = Vector2.zero;
+	private Transform mTrans;
+	private Quaternion mStart;
+	private Vector2 mRot = Vector2.zero;
 
 	void Start ()
 	{
